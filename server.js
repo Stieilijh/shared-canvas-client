3f294b8b4c44a7ec373c3f6bf293e23103052c3d
Stieilijh <saitushar02@gmail.com>
2025-02-26 15:52:58 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  }
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send("Socket.io Server is running!");
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('drawing', (data) => {
+    socket.broadcast.emit('drawing', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
